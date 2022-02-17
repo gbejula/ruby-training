343147ca99a834ba92f97a2537b5787bcb08a0b9
Olugbenga Ajayi <gbejula@gmail.com>
2022-02-18 00:15:08 +0100

    strings learnings

---
+++ strings.rb
@@ -0,0 +1,21 @@
+firstName = "Ibukunoluwa"
+lastName = "Ajayi"
+
+puts firstName + " " + lastName
+
+puts "My daughter's name is #{firstName} #{lastName}"
+
+firstName.class
+
+
+
+# Methods
+
+# 10.class -- tell you the type of class
+
+10.to_s
+
+info = "Gbenga asked, 'Hello Ibukun, how are you doing?'"
+
+print info
+
