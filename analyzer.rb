ab30edf3e4acf62dec389e8d5757a8ce18755ff1
Olugbenga Ajayi <gbejula@gmail.com>
2022-02-18 03:01:04 +0100

    string validation and interpolation

---
+++ analyzer.rb
@@ -0,0 +1,16 @@
+puts "Enter your first name: ?"
+firstname = gets.chomp
+puts "Enter your last name: ?"
+lastname = gets.chomp
+
+fullname = "#{firstname} #{lastname}"
+
+puts fullname
+
+puts "Your full name is #{fullname}"
+puts "Your full name reversed is #{fullname.reverse}"
+puts "Your name has #{fullname.length} characters in it"
+
+
+
+
--- strings2.rb
+++ strings2.rb
@@ -1,3 +1,10 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Welcome to Portugal, #{name}"
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Welcome to Portugal, #{name}"
+
+puts "Enter a number and multiply it by 2"
+input = gets.chomp
+puts input.to_i * 2
+
+
+
