1d7c0b48529b3ed068413b305bf02b98e7f311d5
Olugbenga Ajayi <gbejula@gmail.com>
2022-03-10 23:57:34 +0100

    learning on ruby arrays

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p a
+puts a.last
+
+# unshift == adds to the front of an array
+# push == adds to the back of an array
+# append == add to the back of an array
+# empty? == is used to check if an array is empty
+# include? == is used to check if something is included in an array
+# split == used to split an array
+# join == used to join an array
