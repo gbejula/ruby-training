a60d7a81eabc3075527e208a95b90faf10058ec1
Olugbenga Ajayi <gbejula@gmail.com>
2022-03-10 22:04:09 +0100

    learnt conditionals

--- calculator.rb
+++ calculator.rb
@@ -25,9 +25,27 @@ puts "Please enter your first number"
 first_number = gets.chomp
 puts "Please enter your second number"
 second_number = gets.chomp
-puts "The first number multiplied by the second number is: #{multiply(first_number, second_number)}"
-puts "The first number divided by the second number is: #{divide(first_number, second_number)}"
-puts "The first number added by the second number is: #{add(first_number, second_number)}"
-puts "The first number subtracted by the second number is: #{subtract(first_number, second_number)}"
-puts "The first number modulo the second number is: #{mod(first_number, second_number)}"
-
+# puts "The first number multiplied by the second number is: #{multiply(first_number, second_number)}"
+# puts "The first number divided by the second number is: #{divide(first_number, second_number)}"
+# puts "The first number added by the second number is: #{add(first_number, second_number)}"
+# puts "The first number subtracted by the second number is: #{subtract(first_number, second_number)}"
+# puts "The first number modulo the second number is: #{mod(first_number, second_number)}"
+puts "What do you want to do?"
+puts "Enter 1 for multiply, 2 for addition, 3 for subtraction, 4 for modulo"
+user_entry = gets.chomp
+puts "You selected #{user_entry}"
+if user_entry == "1"
+  puts "You have chosen to multiply"
+  puts "The result is #{multiply(first_number, second_number)}"
+elsif user_entry == "2"
+  puts "You have chosen to add"
+  puts "The result is #{add(first_number, second_number)}"
+elsif user_entry == "3"
+  puts "You have chosen to subtract"
+  puts "The result is #{subtract(first_number, second_number)}"
+elsif user_entry == "4"
+  puts "You have chosen to find the mod"
+  puts "The result is #{mod(first_number, second_number)}"
+else
+  puts "Invalid entry"
+end
---
+++ conditionals.rb
@@ -0,0 +1,20 @@
+# condition = true
+# another_condition = false
+
+# if condition && another_condition
+#   puts "Hello"
+# else
+#   puts "Bye Bye"
+# end
+# puts "Bowwww.."
+
+name = "Gbenga"
+
+name = "Gbenga"
+if name == "Gbenga"
+  puts "Welcome to learning ruby, Gbenga"
+elsif name == "Kola"
+  puts "Welcome to learning ruby, Kola"
+else
+  puts "Welcome to the program, everyone."
+end
